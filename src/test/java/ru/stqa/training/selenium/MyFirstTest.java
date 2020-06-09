736e00feba6722b3f28333f3d75d5f10dea42186
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-06-09 09:11:59 +0300

    done first homework

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
@@ -18,7 +21,8 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "/src/test/resources/drivers/msedgedriver.exe");
+        driver = new EdgeDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
---
+++ src/test/java/ru/stqa/training/selenium/Sandbox.java
@@ -0,0 +1,8 @@
+package ru.stqa.training.selenium;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("chromedriver"));
+    }
+}
